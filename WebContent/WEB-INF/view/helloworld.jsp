2e2aaab9095158829cb689fdca49f028fb14e0ce
amriteshchandan <amriteshchandan@yahoo.in>
2020-03-01 10:26:16 +0530

    Passing data aroung using model object

--- WebContent/WEB-INF/view/helloworld.jsp
+++ WebContent/WEB-INF/view/helloworld.jsp
@@ -9,6 +9,8 @@
 <body>
 Hello World Of Spring!
 <br><br>
-Student Name : ${param.studentName}
+Student Name: ${param.studentName}
+<br><br>
+The message: ${message}
 </body>
 </html>
--- src/com/amritesh/springdemo/mvc/HelloWorldController.java
+++ src/com/amritesh/springdemo/mvc/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.amritesh.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,4 +19,13 @@ public class HelloWorldController {
 		return "helloworld";
 	}
 	
+	// Read form data and add that data in Model
+	@RequestMapping(value="/processFormVersion2")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		String name = request.getParameter("studentName");
+		name = name.toUpperCase();
+		name = "Yo! " + name;
+		model.addAttribute("message", name);
+		return "helloworld";
+	}
 }
