8bc61e23b931097ac740ab1b9505f05395663fbb
amriteshchandan <amriteshchandan@yahoo.in>
2020-03-04 03:09:35 +0530

    Re-INIT

--- src/com/amritesh/springdemo/mvc/HelloWorldController.java
+++ src/com/amritesh/springdemo/mvc/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+// comment
 @Controller
 @RequestMapping("/hello")
 public class HelloWorldController {
